e5ee368768eedf698116f4e31499b1519cc12080
javierpb275 <javierpb275@gmail.com>
2022-02-05 20:44:28 +0100

    signup(), refreshToken() and getProfile() methods created and created routes in user router

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,85 @@
 import { Request, Response } from "express";
+import { generateToken } from "../helpers/authentication.helper";
+import IUser from "../interfaces/user.interface";
+import User from "../models/user.model";
+import config from "../config/config";
+
+let refreshTokens: string[] = [];
 
 class UserController {
-  public async getProfile(req: Request, res: Response): Promise<Response> {
+  //SIGN UP:
+  public async signUp(req: Request, res: Response): Promise<Response> {
+    const newUser: IUser = new User(req.body);
     try {
-      return res.status(200).send("getProfile");
+      const foundUser: IUser | null = await User.findOne({
+        email: req.body.email,
+      });
+      if (foundUser) {
+        return res.status(400).send({ error: "Wrong email. Try again" });
+      }
+      const accessToken: string = generateToken(
+        newUser._id.toString(),
+        config.AUTH.ACCESS_TOKEN_SECRET,
+        "15m"
+      );
+      const refreshToken: string = generateToken(
+        newUser._id.toString(),
+        config.AUTH.REFRESH_TOKEN_SECRET,
+        "20m"
+      );
+      refreshTokens.push(refreshToken);
+      await newUser.save();
+      return res.status(201).send({
+        user: newUser,
+        accessToken,
+        refreshToken,
+      });
+    } catch (err) {
+      return res.status(400).send(err);
+    }
+  }
+
+  //REFESH TOKEN
+  public async refreshToken(req: Request, res: Response): Promise<Response> {
+    try {
+      if (!req.body.token) {
+        return res.status(400).send({ error: "Please, provide refresh token" });
+      }
+      if (!refreshTokens.includes(req.body.token)) {
+        return res.status(400).send({ error: "Refresh Token Invalid" });
+      }
+      refreshTokens = refreshTokens.filter(
+        (reToken) => reToken != req.body.token
+      );
+      const accessToken: string = generateToken(
+        req.body.userId,
+        config.AUTH.ACCESS_TOKEN_SECRET,
+        "15m"
+      );
+      const refreshToken: string = generateToken(
+        req.body.userId,
+        config.AUTH.REFRESH_TOKEN_SECRET,
+        "20m"
+      );
+      refreshTokens.push(refreshToken);
+      return res.status(200).send({
+        accessToken,
+        refreshToken,
+      });
+    } catch (err) {
+      return res.status(500).send(err);
+    }
+  }
+
+  //GET PROFILE
+  public async getProfile(req: Request, res: Response): Promise<Response> {
+    const { userId } = req;
+    try {
+      const user: IUser | null = await User.findOne({ _id: userId });
+      if (!user) {
+        return res.status(404).send({ error: "User Not Found!" });
+      }
+      return res.status(200).send(user);
     } catch (err) {
       return res.status(500).send(err);
     }
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import userController from "../controllers/user.controller";
+import { auth } from "../middlewares/auth.middleware";
 
 const router: Router = Router();
 
-router.get("/me", userController.getProfile);
+router.post("/", userController.signUp);
+router.post("/refreshToken", userController.refreshToken);
+
+router.get("/me", auth, userController.getProfile);
 
 export default router;
