0696d8fdab87bd04b440aec41ac70676d6d2ba60
javierpb275 <javierpb275@gmail.com>
2022-06-04 17:36:42 +0200

    make it better

--- src/routers/hello.router.ts
+++ src/routers/hello.router.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router: Router = Router();
 
 router.get("/", (req, res) => {
-    res.status(200).send("Hello World!")
+  res.status(200).send("Hello World!");
 });
 
 export default router;
